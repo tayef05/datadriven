d2f458a53d21b4edb7815eba205f890b97e9845c
tayef <tayef05@outlook.com>
2022-03-19 00:58:32 +0600

    Delete using HTTP service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -14,12 +14,13 @@ export class PostsComponent implements OnInit {
     });
   }
   url = 'https://jsonplaceholder.typicode.com/posts';
-  posts: any;
+  posts: any = [];
   myForm = this.fb.group({
     userId: [''],
     title: [''],
     body: [''],
   });
+  delete_clicked = 0;
   createPost(obj: any) {
     this.http.post(this.url, obj).subscribe((res) => {
       // set obj at first element of array
@@ -28,6 +29,22 @@ export class PostsComponent implements OnInit {
     });
     // reset myForm value
     this.myForm.reset();
+  }
+  deletePost(id: number) {
+    const new_urls = this.url + '/' + id;
+    const headers = {
+      Authorization: 'Bearer my-token',
+      'My-Custom-Header': 'foobar',
+    };
+    this.http.delete(new_urls, { headers }).subscribe((res) => {
+      console.log('Successfully delete the object with id: ' + id);
+      console.warn(res);
+
+      id = id - this.delete_clicked - 1;
+      this.posts.splice(id, 1);
+      this.delete_clicked++;
+      console.log(id);
+    });
   }
   ngOnInit(): void {}
 }
