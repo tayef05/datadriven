fbff88a75bd7ae1b775fd388751a77f9e6df4f34
tayef <tayef05@outlook.com>
2022-03-19 16:40:49 +0600

    deploy webapp to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBosY4qsyoh6Zt6SPf-Pu3LyRSG95y3t9k',
+    authDomain: 'datadriven-dcbbf.firebaseapp.com',
+    projectId: 'datadriven-dcbbf',
+    storageBucket: 'datadriven-dcbbf.appspot.com',
+    messagingSenderId: '778342423898',
+    appId: '1:778342423898:web:53b02cb61936984c1e6763',
+    measurementId: 'G-WRY5VZHVSV',
+  },
 };
